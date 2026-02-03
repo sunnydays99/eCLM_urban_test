a8cd965bc854bfbb71e69e434bdbdcad561e5dcb
sunnydays99 <150025643+sunnydays99@users.noreply.github.com>
2026-02-03 16:31:52 +0100

    Update initInterp.F90

--- src/clm5/init_interp/initInterp.F90
+++ src/clm5/init_interp/initInterp.F90
@@ -777,29 +777,6 @@ contains
   end subroutine initInterp
 
   !-----------------------------------------------------------------------
-  subroutine copy_and_add_metadata(ncidi, ncido, filei)
-    !
-    ! !DESCRIPTION:
-    ! Copy any necessary global metadata from the input file to the output file
-    !
-    ! !ARGUMENTS:
-    type(file_desc_t), intent(inout) :: ncidi ! input (source) file
-    type(file_desc_t), intent(inout) :: ncido ! output (destination) file
-    character(len=*) , intent(in)    :: filei ! input (source) filename
-
-    !
-    ! !LOCAL VARIABLES:
-    character(len=*), parameter :: subname = 'copy_and_add_metadata'
-    !-----------------------------------------------------------------------
-
-    call ncd_redef(ncido)
-    call ncd_putatt(ncido, ncd_global, 'initial_source_file', trim(filei))
-    call copy_issue_fixed_metadata(ncidi, ncido)
-    call ncd_enddef(ncido)
-
-  end subroutine copy_and_add_metadata
-
-  !-----------------------------------------------------------------------
   function skip_var(iflag_interpinic)
     !
     ! !DESCRIPTION:
