1a8d63f8a3829bd41ef3d0cf508e89ddd029cf5b
sunnydays99 <150025643+sunnydays99@users.noreply.github.com>
2026-05-11 12:21:10 +0200

    Update SurfaceRadiationMod.F90

--- src/clm5/biogeophys/SurfaceRadiationMod.F90
+++ src/clm5/biogeophys/SurfaceRadiationMod.F90
@@ -400,6 +400,14 @@ contains
           nrad        => surfalb_inst%nrad_patch, &       ! number of canopy layers
           parsun_z    => solarabs_inst%parsun_z_patch, &  ! absorbed PAR for sunlit leaves
           parsha_z    => solarabs_inst%parsha_z_patch, &  ! absorbed PAR for shaded leaves
+          ! for leaf APAR
+          fabdl_sun_z  => surfalb_inst%fabdl_sun_z_patch, & ! absorbed sunlit leaf direct PAR
+          fabdl_sha_z  => surfalb_inst%fabdl_sha_z_patch, & ! absorbed shaded leaf direct PAR
+          fabil_sun_z  => surfalb_inst%fabil_sun_z_patch, & ! absorbed sunlit leaf diffuse PAR
+          fabil_sha_z  => surfalb_inst%fabil_sha_z_patch, & ! absorbed shaded leaf diffuse PAR
+		  parlsun_z    => solarabs_inst%parlsun_z_patch, &  ! absorbed PAR for sunlit leaves
+          parlsha_z    => solarabs_inst%parlsha_z_patch, &  ! absorbed PAR for shaded leaves
+
           laisun      => canopystate_inst%laisun_patch, & ! sunlit leaf area
           laisha      => canopystate_inst%laisha_patch, & ! shaded  leaf area
           laisun_z    => canopystate_inst%laisun_z_patch, & ! sunlit leaf area for canopy layer
@@ -415,6 +423,10 @@ contains
            parsha_z(p,iv) = 0._r8
            laisun_z(p,iv) = 0._r8
            laisha_z(p,iv) = 0._r8
+           ! for leaf APAR
+           parlsun_z(p,iv) = 0._r8
+           parlsha_z(p,iv) = 0._r8
+
         end do
         
         ! Loop over patches to calculate laisun_z and laisha_z for each layer.
@@ -445,6 +457,10 @@ contains
         do iv = 1, nrad(p)
            parsun_z(p,iv) = forc_solad(g,ipar)*fabd_sun_z(p,iv) + forc_solai(g,ipar)*fabi_sun_z(p,iv)
            parsha_z(p,iv) = forc_solad(g,ipar)*fabd_sha_z(p,iv) + forc_solai(g,ipar)*fabi_sha_z(p,iv)
+           ! for leaf APAR
+           parlsun_z(p,iv) = forc_solad(g,ipar)*fabdl_sun_z(p,iv)*0.87 + forc_solai(g,ipar)*fabil_sun_z(p,iv)*0.87
+           parlsha_z(p,iv) = forc_solad(g,ipar)*fabdl_sha_z(p,iv)*0.87 + forc_solai(g,ipar)*fabil_sha_z(p,iv)*0.87
+
         end do
         
      end do ! end of fp = 1,num_nourbanp loop
